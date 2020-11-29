65162089f5bb7fa9a5d13145e7a127c86e716756
MahadarshiniGopinath <74991080+MahadarshiniGopinath@users.noreply.github.com>
2020-11-29 17:04:44 +0000

    dfsdf

--- CanteenSystem.Web/ViewModel/OrderConfirmationModel.cs
+++ CanteenSystem.Web/ViewModel/OrderConfirmationModel.cs
@@ -1,11 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace CanteenSystem.Web.ViewModel
 {
     public class OrderConfirmationModel
     {
+        public OrderConfirmationModel(string message)
+        {
+            NotificationMessage = message;
+        }
+
+        public string NotificationMessage { get; set; }
+        public string Status { get; set; }
+
     }
 }
