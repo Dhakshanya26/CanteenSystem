b7741a346dcb5e5c1678534f579c0c1f2aab7204
MahadarshiniGopinath <74991080+MahadarshiniGopinath@users.noreply.github.com>
2020-11-29 17:03:13 +0000

    jhgh

---
+++ CanteenSystem.Web/ViewModel/OrderConfirmationModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CanteenSystem.Web.ViewModel
+{
+    public class OrderConfirmationModel
+    {
+    }
+}
